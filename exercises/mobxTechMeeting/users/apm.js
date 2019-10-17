fa73dd02b342985b82c684ae9c466e54e56ed0a1
Davide D'Antonio <davide.dantonio@webeetle.com>
2019-10-17 17:12:42 +0200

    removed mobx example

--- exercises/mobxTechMeeting/users/apm.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const isDocker = require('is-docker')
-
-require('elastic-apm-node').start({
-  serverUrl: `http://${isDocker() ? 'apm-server' : 'localhost'}:8200`,
-})
--- exercises/mobxTechMeeting/users/src/hooks/preHandler.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const fp = require('fastify-plugin')
-
-module.exports = fp(async (fastify, opts) => {
-  fastify.addHook('preHandler', async (request, reply) => {
-    return request.jwtVerify()
-  })
-})
